945d1aad5e3eb437e258f659eb60cdd43d615685
elevenBeans <elevenbeansf2e@gmail.com>
2018-04-11 03:26:05 +0800

    mbd

--- 6-zigzag-conversion/index.js
+++ 6-zigzag-conversion/index.js
@@ -21,24 +21,39 @@ var convert = function(s, numRows) {
   var i = 0;
   var arr = [];
   var count = 1;
+  var c = 1;
+  var res = [];
+  function copy(s, len) {
+    var res = '';
+    for(var i = 0; i< len; i++) {
+      res = res + s;
+    };
+    return res;
+  }
   while(i < s.length) {
     if( count % 2 !== 0 ) {
       arr.push(s.substring(i, i + numRows));
-      // swt = false;
       i += numRows;
       count++;
     } else {
       if (i % (2*(numRows-1)) === 0) {
         count++;
+        c = 1;
       } else {
         var temp = s.split('')[i];
-        // temp = temp + ;
+        temp = copy('#', numRows - c - 1 )+ temp + copy('#', c);
         arr.push(temp);
         i++;
+        c++;
       }
     }
   }
-  return arr;
+  for(var i = 0; i < numRows; i++) {
+    arr.map((item, index) => {
+      res.push(item[i]);
+    });
+  };
+  return res.join('').replace(/#/g, '');
 };
 // 2-2
 // 3-4
@@ -47,16 +62,24 @@ var convert = function(s, numRows) {
 // 6-10
 //
 
-// 0    6    12
-// 1  5 7   11
-// 2 4  8 10
-// 3    9
+// 3
+// 0   4   8   12   16
+// 1 3 5 7 9 1113 1517
+// 2   6   10  14
 
+// 4
+// 0    6     12    18
+// 1  5 7   1113  17
+// 2 4  8 10  14 16
+// 3    9     15
 
+// i - ((numRows - 1) + count * )
+
+// 5
 // 0     8      16
 // 1   7 9    15
 // 2  6 10  14
 // 3 5  11 13
 // 4    12
 
-console.log(convert('PAYALISHIRING', 3));
+console.log(convert('PAYPALISHIRING', 3));
